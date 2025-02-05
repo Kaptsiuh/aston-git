864c1ea49f77daa5a10815dab8aa972423978170
Dima Berry <dimaberry1@gmail.com>
2025-02-05 20:57:24 +0100

    Added new people to the people array

--- index.js
+++ index.js
@@ -12,7 +12,14 @@ class Person {
   }
 }
 
-const people = [new Person("Dima"), new Person("Kris")];
+const people = [
+  new Person("Dima"),
+  new Person("Kris"),
+  new Person("Alex"),
+  new Person("John"),
+  new Person("Bob"),
+  new Person("Mike"),
+];
 
 people.forEach((p) => {
   p.sayHello();
