2fd9854ed87bf97ef8672b1fafdc556a46665c8d
Dima Berry <dimaberry1@gmail.com>
2025-02-05 20:16:24 +0100

    Added method sayGoodbye in Person class

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ class Person {
   sayHello() {
     console.log(`Hello, my name is ${this.name}!`);
   }
+
+  sayGoodbye() {
+    console.log(`See you soon, ${this.name}!`);
+  }
 }
 
 const people = [new Person("Dima"), new Person("Kris")];
 
-people.forEach((p) => p.sayHello());
+people.forEach((p) => {
+  p.sayHello();
+  p.sayGoodbye();
+});
